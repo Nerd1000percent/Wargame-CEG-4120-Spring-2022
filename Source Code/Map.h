f4acdcf0b2befec57c867d629fefbf7ae7befaa8
Chris <kemplin.christopher@gmail.com>
2022-02-20 13:25:06 -0500

    movement first steps
    I believe this is all of the movement code I can do for now.  The rest will take having the other portions of the phase.

--- Source Code/Map.h
+++ Source Code/Map.h
@@ -2,20 +2,31 @@
 #ifndef MAP-H
 #define MAP-H
 #pragma once
+#include "Unit.h"
+#include "Tiles.h"
+#include<list>
 #include<string>
 #include<iostream>
 using namespace std;
 class Map {
 //Attributes
 private:
-   int arrayOfTiles[][];
+   Tiles arrayOfTiles[][];
    int mapDimension;
+   list<Unit*> engagedUnits;
 //Member functions
 public:
    //Constructor
    Map();
+   Map(const Map clone);
    //Getters
    int arrayOfTilesGetter();
-
+   void saveState(istream log);
+   void moveUnit(string unitId, int locCoords[2], int destinationCoords[2]);
+   //TODO void resolveCombat();
+   //TODO void findEgnagedUnits();
+private:
+	bool hostileCheck(int coords[2]);
+	bool checkPath(Unit* unit, int locCoords[2], int destinationCoords[2]);
 };
 #endif

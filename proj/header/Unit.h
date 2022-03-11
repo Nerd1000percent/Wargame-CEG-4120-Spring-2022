38169079cdd537cfc9bccab0742da5a16aeff007
blinger <robert.franklin.linger@gmail.com>
2022-03-11 13:31:51 -0500

    Changed documentation to doxygen format

--- proj/header/Unit.h
+++ proj/header/Unit.h
@@ -5,9 +5,8 @@
 #include <vector>
 #include <memory>
 
-/*
-* Unit class
-* This class represents combat units in the game
+/**
+* @brief This class represents combat units in the game
 * Unit objects have a string identifier, an active flag, a set number of allowed moves per turn, and attack and defense values for combat
 */
 class Unit
@@ -16,89 +15,119 @@ class Unit
 private:
 
    // member variables
-   std::string m_ID; // string identifier
-   bool m_active; // indicates whether unit is active (i.e. whether it's still in the game)
-   int m_numMoves; // number of moves the unit may make per turn
-   int m_currentMoves; // number of moves the unit has left on the current turn
+
+   /**
+   * string identifier
+   */
+   std::string m_ID;
+
+   /**
+   * indicates whether unit is active (i.e. whether it's still in the game)
+   */
+   bool m_active;
+
+   /**
+   * number of moves the unit may make per turn
+   */
+   int m_numMoves;
+
+   /**
+   * number of moves the unit has left on the current turn
+   */
+   int m_currentMoves;
+
+   /**
+   * attack value
+   */
    double m_attackPower;
+
+   /**
+   * defense value
+   */
    double m_defensePower;
 
 public:
    
    // Constructors
-   
-   // Function: Unit constructor
-   // Parameters:
-   //             std::string id - string identifier
-   //             int numMoves - number of moves per turn
-   //             double attackPower - attack value for combat
-   //             double defensePower - defense value for combat
-   // Returns: N/A
+
+   /**
+   * creates new Unit objects
+   * @param id String identifier
+   * @param numMoves Number of moves per turn
+   * @param attackPower attack value for combat
+   * @param defensePower defense value for combat
+   */
    Unit(std::string id, int numMoves, double attackPower, double defensePower);
 
-   // Function: Unit copy constructor
-   // Parameters: Unit& other - reference to the Unit object to be copied
-   // Returns: N/A
+   /**
+   * creates copies of Unit objects
+   * @param other Reference to the Unit object to be copied
+   */
    Unit(Unit& other);
 
    // Getter functions
 
-   // Function: getID
-   // Parameters: N/A
-   // Returns: std::string - m_id
+   /**
+   * gets m_id
+   * @returns m_id
+   */
    std::string getID();
 
-   // Function: isactive
-   // Parameters: N/A
-   // Returns: bool - m_active
+   /**
+   * gets m_active
+   * @returns m_active
+   */
    bool isActive();
 
-   // Function: getMovement
-   // Parameters: N/A
-   // Returns: int - m_numMoves
+   /**
+   * gets m_numMoves
+   * @returns m_numMoves
+   */
    int getMovement();
 
-   // Function: getCurrentMovement
-   // Parameters: N/A
-   // Returns: int - m_currentMoves
+   /**
+   * gets m_currentMoves
+   * @returns m_currentMoves
+   */
    int getCurrentMovement();
 
-   // Function: getAttackPower
-   // Parameters: N/A
-   // Returns: double - m_attackPower
+   /**
+   * gets m_attackPower
+   * @returns m_attackPower
+   */
    double getAttackPower();
 
-   // Function: getDefensePower
-   // Parameters: N/A
-   // Returns: double - m_defensePower
+   /**
+   * gets m_defensePower
+   * @returns m_defensePower
+   */
    double getDefensePower();
 
    // Unit action functions
 
-   // Function: dealDamage
-   // Parameters: double damage - damage value to be deducted from the Unit's attack and defense values
-   // Returns: N/A
-   // Side Effects: if either m_attackValue or m_defenseValue hits 0, m_active will be set to false
+   /**
+   * deducts damage from Unit's attack and defense then sets m_active to false if either reach 0
+   * @param damage value to be deducted from the Unit's attack and defense values
+   * @returns void
+   */
    void dealDamage(double damage);
 
-   // Function: spendMovement
-   // Parameters: int cost - number of movement points to be used
-   // Returns: N/A
-   // Side Effects: cost is deducted from m_currentMovement
+   /**
+   * deducts cost from m_currentMoves
+   * @param cost The number to be deducted from m_currentMoves
+   */
    void spendMovement(int cost);
 
-   // Function: resetMovement
-   // Parameters: N/A
-   // Returns: N/A
-   // Side Effects: m_currentMoves is set to the value of m_numMoves
+   /**
+   * sets m_currentMoves to the value of m_numMoves
+   */
    void resetMovement();
 
-   // Debugging function
+   // Debugging functions
 
-   // Function: printUnit
-   // Parameters: N/A
-   // Returns: N/A
-   // Side Effects: prints out the member variables of the Unit object
+   /**
+   * prints out the member variables
+   */
    void printUnit();
 };
 #endif

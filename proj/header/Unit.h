cc415d0cddc18b532084bd9dd989778cfa142036
blinger <robert.franklin.linger@gmail.com>
2022-03-11 12:14:12 -0500

    Removed engagement list and functions that used it

--- proj/header/Unit.h
+++ proj/header/Unit.h
@@ -12,26 +12,22 @@ private:
    std::string m_ID;
    bool m_active;
    int m_numMoves;
+   int m_currentMoves; // To decrease as a unit moves around the map
    double m_attackPower;
    double m_defensePower;
-   int m_currentMoves; // To decrease as a unit moves around the map
-   std::vector<std::shared_ptr<Unit>> m_engagementList;
 public:
    
    Unit(std::string id, int numMoves, int attackPower, int defensePower);
    Unit(Unit& other);
-   Unit(Unit& destroyedUnit, bool active);
 
    std::string getID();
    bool isActive();
    int getMovement();
+   int getCurrentMovement();
    double getAttackPower();
    double getDefensePower();
-   int getCurrentMovement();
 
    void dealDamage(double damage);
-   void engageUnit(std::shared_ptr<Unit> enemy);
-   void disengageUnit(std::string unitID);
    void spendMovement(int cost);
    void printUnit();
    void resetMovement();

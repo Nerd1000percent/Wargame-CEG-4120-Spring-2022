56deed6db9072880dbd4306c815a40ec30007e7b
blinger <robert.franklin.linger@gmail.com>
2022-03-12 14:55:45 -0500

    Began writing tests for Map

--- test/MapTest.cpp
+++ test/MapTest.cpp
@@ -18,27 +18,12 @@ public:
     
     // clear database
     UnitDatabase::getUnitDatabase().clear();
-
-    // build a tile
-    Tiles tile("terrainName", 1);
-    UnitDatabase::getUnitDatabase().addUnit(std::make_shared<Unit>("unit1", 1, 2, 3));
-    UnitDatabase::getUnitDatabase().addUnit(std::make_shared<Unit>("unit2", 4, 5, 6));
-    tile.addUnit(UnitDatabase::getUnitDatabase().getUnit("unit1"));
-    tile.addUnit(UnitDatabase::getUnitDatabase().getUnit("unit2"));
-
-    // create a map with the tile
-    Coordinates dimensions(1, 1);
-    p_map = std::make_shared<Map>(dimensions);
-    //p_map->
   }
 
   // Override this to define how to tear down the environment.
   void TearDown() override {
     UnitDatabase::getUnitDatabase().clear();
-    p_map.reset();
   }
-
-  shared_ptr<Map> p_map;
 };
 
 TEST_F(MapTestSuite, testSize)
@@ -67,7 +52,7 @@ TEST_F(MapTestSuite, serializationTest)
   // serialize the map
   nlohmann::json j = orig;
 
-  cout << j.dump() << endl;
+  //cout << j.dump() << endl;
 
   // deserialze the map
   Map copy({ 0, 0 });

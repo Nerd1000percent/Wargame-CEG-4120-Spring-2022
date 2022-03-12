bbdbdadc81b8e544c96938832bf887872f1c241f
blinger <robert.franklin.linger@gmail.com>
2022-03-12 12:49:17 -0500

    Added a line to clear the UnitDatabase in both the Setup and Teardown functions

--- test/MapTest.cpp
+++ test/MapTest.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 
 #include "Map.h"
+#include "UnitDatabase.h"
 
 #include <memory>
 using namespace std;
@@ -13,12 +14,14 @@ class MapTestSuite : public testing::Test
 public:
   // Override this to define how to set up the environment.
   void SetUp() override {
-    pMap = std::make_shared<Map>(5);
+    //pMap = std::make_shared<Map>(5);
+    UnitDatabase::getUnitDatabase().clear();
   }
 
   // Override this to define how to tear down the environment.
   void TearDown() override {
-    pMap.reset();
+    //pMap.reset();
+    UnitDatabase::getUnitDatabase().clear();
   }
 
   std::shared_ptr<Map> pMap;

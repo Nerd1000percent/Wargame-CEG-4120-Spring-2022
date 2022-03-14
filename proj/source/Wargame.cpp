4f6d1d409aacf5b4ef04767ba0debe2b7e812250
blinger <robert.franklin.linger@gmail.com>
2022-03-13 20:02:14 -0400

    Game now reads in map files

--- proj/source/Wargame.cpp
+++ proj/source/Wargame.cpp
@@ -1,22 +1,36 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 #include "nlohmann/json.hpp"
 
-using namespace std;
+#include "Map.h"
 
-//Map loadMap()
-//{
-//    
-//}
-//
-//void processMoveCommand()
-//{
-//
-//}
+const std::string FILENAME = "inputMap.json";
+
+Map loadMap()
+{
+    // Load the map
+    std::ifstream inFile(FILENAME);
+    //std::cout << "The file is open: " << inFile.is_open() << std::endl;
+    nlohmann::json j;
+    inFile >> j;
+    //std::cout << j.dump() << endl;
+    Map gameBoard({ 0, 0 });
+    j.get_to(gameBoard);
+    return gameBoard;
+}
+
+void processMoveCommand()
+{
+
+}
 
 int main(int argc, char* argv[])
 {
     
+    Map gameBoard = loadMap();
+
 
     return 0;
 }

a53bcd47e12636936d751b83f867b3bbf11dbfc1
blinger <robert.franklin.linger@gmail.com>
2022-03-13 21:51:27 -0400

    Console now prompts user to specify an input file to load the map from

--- proj/source/Wargame.cpp
+++ proj/source/Wargame.cpp
@@ -7,43 +7,62 @@
 
 #include "Map.h"
 
-const std::string INPUT_FILE = "inputMap.json";
-const std::string OUTPUT_FILE = "outputMap.html";
-const std::string GAME_FILE = "gameState.json";
+const std::string MAP_DIR = "MapFiles\\";
+const std::string HTML_FILE = MAP_DIR + "currentState.html";
 
-Map loadMap()
+bool loadMap(std::string inputFile, std::shared_ptr<Map> p_map)
 {
-    // Load the map
-    std::ifstream inFile(INPUT_FILE);
-    //std::cout << "The file is open: " << inFile.is_open() << std::endl;
+    std::ifstream inFile(inputFile);
+    if (!inFile.is_open()) return false;
     nlohmann::json j;
     inFile >> j;
+    j.get_to(*p_map);
+    return true;
+
+    //// load
+    //std::ifstream inFile(INPUT_FILE);
+    //std::cout << "The file is open: " << inFile.is_open() << std::endl;
+    //nlohmann::json j;
+    //inFile >> j;
     //std::cout << j.dump() << endl;
-    Map gameBoard({ 0, 0 });
-    j.get_to(gameBoard);
-    return gameBoard;
+    //Map gameBoard({ 0, 0 });
+    //j.get_to(gameBoard);
+    //return gameBoard;
 }
 
-void displayMap(std::unique_ptr<Map> gameBoard)
+void displayMap(std::shared_ptr<Map> p_map)
 {
     std::ofstream mapOutput;
-    mapOutput.open(OUTPUT_FILE);
-    mapOutput << gameBoard->mapToHtml();
+    mapOutput.open(HTML_FILE);
+    mapOutput << p_map->mapToHtml();
 }
 
-void saveGame(std::unique_ptr<Map> gameBoard)
-{
-    ofstream jsonOputput;
-    jsonOputput.open(GAME_FILE);
-    nlohmann::json j = *gameBoard;
-    jsonOputput << std::setw(4) << j << std::endl;
-}
+//void saveGame(std::string saveFile, Map& map)
+//{
+//    ofstream jsonOutput;
+//    jsonOutput.open(saveFile);
+//    nlohmann::json j = map;
+//    jsonOutput << std::setw(4) << j << std::endl;
+//
+//    //// L
+//    //ofstream jsonOutput;
+//    //jsonOutput.open(GAME_FILE);
+//    //nlohmann::json j = *gameBoard;
+//    //jsonOutput << std::setw(4) << j << std::endl;
+//}
 
 int main(int argc, char* argv[])
 {
-    
-    Map gameBoard = loadMap();
-    displayMap(std::make_unique<Map>(gameBoard));
+    std::cout << "Enter the name of the file to load the game board from: " << std::endl;
+    std::string inputFile;
+    std::cin >> inputFile;
+    inputFile = MAP_DIR + inputFile;
+    auto p_gameBoard = std::make_shared<Map>(Coordinates{ 0, 0 });
+    loadMap(inputFile, p_gameBoard);
+    displayMap(p_gameBoard);
+
+    //Map gameBoard = loadMap();
+    //displayMap(std::make_unique<Map>(gameBoard));
 
     return 0;
 }
